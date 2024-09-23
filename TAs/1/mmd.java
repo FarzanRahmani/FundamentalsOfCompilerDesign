8b89fa88e017f15adfeb71ebf8838c44e72af7df
Farzan <74285751+FarzanRahmani@users.noreply.github.com>
2024-09-23 21:06:04 +0330

    TA codes

---
+++ TAs/1/mmd.java
@@ -0,0 +1,22 @@
+// Hello world this is a simple java program 
+
+/* abc
+ * def ghijk lmnop
+ * qrstuv wxyz
+ * 99521271
+ */
+
+// Hello word class 
+class HelloWorld {
+
+    /* main function */
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World! It's Farzan Rahmani");
+    }
+}
+
+/*
+    GoodBye World
+    Farzan Rahmani
+*/
